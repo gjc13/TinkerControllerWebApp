08f4f22278fac3bfdd638482edeafb69caadbe9e
gjc13 <amtcmp@126.com>
2015-04-25 18:19:45 +0800

    use Http request rather than socket io to improve latencu

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template,url_for
-from flask.ext.socketio import SocketIO, send, emit, join_room, leave_room,disconnect
+from flask import Flask, render_template,url_for,request
+#from flask.ext.socketio import SocketIO, send, emit, join_room, leave_room,disconnect
 import client
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
 global currentuser 
 currentuser = 01
 @app.route('/')
@@ -21,6 +21,17 @@ def touch():
 @app.route('/controller')
 def control():
     return render_template("Controller.html")
+
+@app.route('/command/<cmd_data>')
+def command(cmd_data):
+    data_list = cmd_data.split(',')
+    x = float(data_list[0])
+    y = float(data_list[1])
+    theta = float(data_list[2])
+    client.send("SetRobotSpeed Vx "+str(int(y)))
+    client.send("SetRobotSpeed Vy "+str(int(x)))
+    client.send("SetRobotSpeed Omega "+str(int(-theta)))
+    return ""
 
 
 #@socketio.on('join')
@@ -40,27 +51,28 @@ def control():
 #    send(username + ' has left the room.', room=room)
 
 
-@socketio.on('my event')
-def handle_my_custom_event(json):
-    global currentuser
-    x = json[u'x']
-    y = json[u'y']
-    theta = json[u'theta']
-    if (json[u'user'] == currentuser):
-        print x,y,theta
-        client.send("SetRobotSpeed Vx "+str(int(x)))
-        client.send("SetRobotSpeed Vy "+str(int(y)))
-        client.send("SetRobotSpeed Omega "+str(int(theta)))
-    
-@socketio.on('user')
-def handle_user_event(json):
-    print json[u'user'] , json[u'data']
-    if json[u'data'] == "A user connected!":
-        global currentuser
-        currentuser = json[u'user']
-        print 'current user is ', currentuser
+# @socketio.on('my event')
+# def handle_my_custom_event(json):
+#     global currentuser
+#     x = json[u'x']
+#     y = json[u'y']
+#     theta = json[u'theta']
+#     if (json[u'user'] == currentuser):
+#         print x,y,theta
+#         client.send("SetRobotSpeed Vx "+str(int(y)))
+#         client.send("SetRobotSpeed Vy "+str(int(x)))
+#         client.send("SetRobotSpeed Omega "+str(int(-theta)))
+#     
+# @socketio.on('user')
+# def handle_user_event(json):
+#     print json[u'user'] , json[u'data']
+#     if json[u'data'] == "A user connected!":
+#         global currentuser
+#         currentuser = json[u'user']
+#         client.send("EnableSystem")
+#         print 'current user is ', currentuser
     
 
 if __name__ == '__main__':
-    client.send("EnableSystem")
-    socketio.run(app,host='0.0.0.0')
+    app.run(host = '0.0.0.0', debug = True)
+#    socketio.run(app,host='0.0.0.0')
